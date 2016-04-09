955b8f62ad45861379521b019ca976b8b70554d7
Dominic Prescod <dominicprescod@gmail.com>
2016-04-09 14:00:07 -0400

    angular

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-console.log('hikeit');
+var app = angular.module('hikeit', ["ngRoute"]);
+
+
+app.config(["$routeProvider","$locationProvider", function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode({enabled: true});
+
+$routeProvider.when('/',{
+  templateUrl: "../partials/first.html",
+  controller:  "indexController",
+  controllerAs: "index"
+});
+
+}]);
+
+app.controller('indexController', ["$http", function($http){
+  var controller = this;
+}]);
